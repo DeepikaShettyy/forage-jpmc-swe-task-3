b6a3e89be80e19f848d561fdfc8bfb151071ff18
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-17 00:41:29 +0200

    Task 3: Update - schema matchup

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,9 +1,15 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  //stock: string,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  //top_ask_price: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
